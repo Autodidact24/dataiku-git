6a9da55793dc8bb57066da2f4aedd490a78102d3
admin <admin>
2022-10-08 15:19:47 +0530

    Saved recipe: CLEANAIRORNOT.compute_Dv0sfj8P

--- recipes/compute_Dv0sfj8P.py
+++ recipes/compute_Dv0sfj8P.py
@@ -2,6 +2,8 @@
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+from fastdownload import download_url
+from fastcore.all import *
 from time import sleep
 
 # Read recipe inputs
