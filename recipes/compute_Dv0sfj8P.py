b3dc23c463de2594cd4f2fc34bdf82f021758a99
admin <admin>
2022-10-08 15:18:21 +0530

    Saved recipe: CLEANAIRORNOT.compute_Dv0sfj8P

--- recipes/compute_Dv0sfj8P.py
+++ recipes/compute_Dv0sfj8P.py
@@ -2,6 +2,7 @@
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+from time import sleep
 
 # Read recipe inputs
 clean_air_or_not = dataiku.Folder("TPQ6w2ke")
@@ -12,6 +13,20 @@ from fastcore.all import *
 
 def search_images(term, max_images=200): return L(ddg_images(term, max_results=max_images)).itemgot('image')
 
+searches = 'clean air skyline','polluted air skyline'
+path = Path('clean air or not')
+
+
+for o in searches:
+    dest = (path/o)
+    dest.mkdir(exist_ok=True, parents=True)
+    download_images(dest, urls=search_images(f'{o} photo'))
+    sleep(10)  # Pause between searches to avoid over-loading server
+    download_images(dest, urls=search_images(f'{o} aqi'))
+    sleep(10)
+    download_images(dest, urls=search_images(f'{o} buildings'))
+    sleep(10)
+    resize_images(path/o, max_size=400, dest=path/o)
 
 # Write recipe outputs
 clean_air = dataiku.Folder("Dv0sfj8P")
