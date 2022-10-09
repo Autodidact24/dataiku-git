e6971b29252cb462d5c1cd14d83368861fa78f7e
admin <admin>
2022-10-09 09:29:17 +0530

    Saved recipe: CLEANAIRORNOT.compute_labelled_images

--- recipes/compute_labelled_images.py
+++ recipes/compute_labelled_images.py
@@ -12,20 +12,19 @@ paths = dataiku.Folder("ep3MDqmT").get_path(), dataiku.Folder("EP5fW8G4").get_pa
 # TODO: Write here your actual code that computes the outputs
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
 
-image_labels_df = ... # Compute a Pandas dataframe to write into image_labels
 
 LABEL_0 = "clean_air"
 LABEL_1 = "polluted_air"
 
-df = pd.DataFrame(columns=['path', 'label'])
+image_labels_df = pd.DataFrame(columns=['path', 'label'])
 for i,j in enumerate(paths):
     if LABEL_0 in j:
-        df.loc[i] = [j[1:], LABEL_0]
+        image_labels_df.loc[i] = [j[1:], LABEL_0]
     if LABEL_1 in j:
-        df.loc[i] = [j[1:], LABEL_1]
+        image_labels_df.loc[i] = [j[1:], LABEL_1]
 
 
 
 # Write recipe outputs
-image_labels = dataiku.Dataset("image_labels")
+image_labels = dataiku.Dataset("labelled_images")
 image_labels.write_with_schema(image_labels_df)
